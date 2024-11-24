647e4bd77eb548eef3ae6c2339c712cc7818294b
Keith John Hutchison <keith@bd2l.com.au>
2024-11-24 16:50:12 +1030

    Added reflection routines from internal project.
    Changes to be committed:
    	modified:   nullable/ErrorMessage.go
    	modified:   nullable/Nullable.go

--- nullable/ErrorMessage.go
+++ nullable/ErrorMessage.go
@@ -3,11 +3,13 @@ package nullable
 import "encoding/json"
 
 type ErrorMessage struct {
-	Attempted string      `json:"attemped,omitempty"`
-	Details   interface{} `json:"details,omitempty"`
-	ErrorNo   int         `json:"errorNo,omitempty"`
-	Message   string      `json:"message"`
-	Stack     []string    `json:"stack,omitempty"`
+	Attempted  string      `json:"attemped,omitempty"`
+	Details    interface{} `json:"details,omitempty"`
+	ErrorNo    int         `json:"errorNo,omitempty"`
+	Function   string      `json:"function,omitempty"`
+	InnerError interface{} `json:"err,omitempty"`
+	Message    string      `json:"message,omitempty"`
+	Stack      []string    `json:"stack,omitempty"`
 }
 
 func (e ErrorMessage) Error() string {
--- nullable/Nullable.go
+++ nullable/Nullable.go
@@ -1,6 +1,7 @@
 package nullable
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"reflect"
@@ -9,12 +10,126 @@ import (
 )
 
 const (
-	BAD_REQUEST      = `bad request`
-	MODIFY_READ_ONLY = `modify read only`
-	NIL_POINTER      = `nil pointer`
-	SET_DATA         = `set data`
-	SET_NULLABLE     = `set nullable`
+	BAD_REQUEST               = `bad request`
+	COMMA                     = `,`
+	LEFT_SQUARE_BRACKET       = `[`
+	JSON                      = `json`
+	NOTHING                   = ``
+	MODIFY_READ_ONLY          = `modify read only`
+	MODIFIED                  = `modified`
+	NIL_POINTER               = `nil pointer`
+	READONLY                  = `read_only`
+	SET_DATA                  = `set data`
+	SET_NULLABLE              = `set nullable`
+	UNEXPECTED_ERROR          = `unexpected error`
+	VARIABLE_MUST_BE_A_STRUCT = `variable must be a struct`
 )
+
+// Nullable represents data that also can be NULL
+type Nullable[T any] struct {
+	Data     T
+	Modified bool
+	ReadOnly bool
+	Selected bool
+	Valid    bool
+}
+
+// CopyLeftToRight
+func CopyLeftToRight(left, right reflect.Value, keepRight bool, setRightSelectedFalse bool, nullablesOnly bool, fields []string) error {
+
+	functionName := `CopyLeftToRight`
+
+	// Dereference pointers if necessary
+	if left.Kind() == reflect.Ptr {
+		left = left.Elem()
+	}
+	if right.Kind() == reflect.Ptr {
+		right = right.Elem()
+	}
+
+	// Ensure both left and right are structs
+	if left.Kind() != reflect.Struct || right.Kind() != reflect.Struct {
+		return errors.New("left and right must be structs")
+	}
+
+	leftType := left.Type()
+	for i := 0; i < left.NumField(); i++ {
+
+		field := leftType.Field(i)
+		fieldName := strings.Split(field.Tag.Get(`json`), COMMA)[0]
+		if fieldName == NOTHING {
+			fieldName = field.Name
+		}
+
+		leftField := left.Field(i)
+		rightField := right.Field(i)
+
+		if leftField.Kind() == reflect.Ptr {
+			leftField = leftField.Elem()
+		}
+		if rightField.Kind() == reflect.Ptr {
+			rightField = rightField.Elem()
+		}
+
+		process := len(fields) == 0
+		if len(fields) > 0 {
+			process = FieldsContainsName(fields, fieldName)
+		}
+		if !process {
+			continue
+		}
+
+		if IsNullable(leftField) && IsNullable(rightField) {
+
+			// Ensure both fields are structs
+			if leftField.Kind() != reflect.Struct || rightField.Kind() != reflect.Struct {
+				continue
+			}
+			rightSelected := rightField.FieldByName("Selected").Bool()
+
+			if rightSelected {
+				if setRightSelectedFalse {
+					err := SetNullableField(false, "Selected", rightField)
+					if err != nil {
+						message := ErrorMessage{
+							Attempted: `SetValueToNullableField`,
+							Details:   err.Error(),
+							ErrorNo:   http.StatusBadRequest,
+							Function:  functionName,
+							Message:   `setRightSelectedFalse`,
+						}
+						return message
+					}
+				}
+				if keepRight {
+					continue
+				}
+			}
+			err := setReflectValueFromToField(leftField, rightField, "Data")
+			if err != nil {
+				message := ErrorMessage{Message: UNEXPECTED_ERROR, Details: "Data", Attempted: `setReflectValueFromToField`, InnerError: err}
+				return message
+			}
+			err = setReflectValueFromToField(leftField, rightField, "Valid")
+			if err != nil {
+				message := ErrorMessage{Message: UNEXPECTED_ERROR, Details: "Valid", Attempted: `setReflectValueFromToField`, InnerError: err}
+				return message
+			}
+
+		} else if !nullablesOnly {
+			// process non nullables and primatives
+			if leftField.IsValid() {
+				if rightField.IsValid() {
+					if rightField.CanSet() {
+						rightField.Set(leftField)
+					}
+				}
+			}
+		}
+	}
+
+	return nil
+}
 
 func fieldNameIsInFields(fieldName string, fields []string) bool {
 	for i := 0; i < len(fields); i++ {
@@ -25,10 +140,9 @@ func fieldNameIsInFields(fieldName string, fields []string) bool {
 	return false
 }
 
-func hasField(typ reflect.Type, fieldName string) bool {
-	for i := 0; i < typ.NumField(); i++ {
-		field := typ.Field(i)
-		if field.Name == fieldName {
+func FieldsContainsName(fields []string, name string) bool {
+	for _, field := range fields {
+		if strings.EqualFold(name, field) {
 			return true
 		}
 	}
@@ -151,13 +265,380 @@ func GetSelectedFields(v interface{}, fields []string) map[string]interface{} {
 	return result
 }
 
-// Nullable represents data that also can be NULL
-type Nullable[T any] struct {
-	Data     T
-	Modified bool
-	ReadOnly bool
-	Selected bool
-	Valid    bool
+func getTag(field reflect.StructField) string {
+	return strings.Split(field.Tag.Get(JSON), COMMA)[0]
+}
+
+func hasField(typ reflect.Type, fieldName string) bool {
+	for i := 0; i < typ.NumField(); i++ {
+		field := typ.Field(i)
+		if field.Name == fieldName {
+			return true
+		}
+	}
+	return false
+}
+
+// IsNullable
+func IsNullable(model reflect.Value) bool {
+	name := strings.Split(model.Type().Name(), LEFT_SQUARE_BRACKET)[0]
+	return name == `Nullable`
+}
+
+// Modified returns true if one or more Nullable fields are selected. Otherwise false is returned
+func Modified(model any) bool {
+
+	// Get the type and inputValue of the input data
+	inputValue := reflect.ValueOf(model)
+
+	if inputValue.Kind() == reflect.Ptr {
+		inputValue = inputValue.Elem()
+	}
+
+	if inputValue.Kind() == reflect.Struct {
+
+		var field reflect.Value
+
+		for i := 0; i < inputValue.NumField(); i++ {
+
+			field = inputValue.Field(i) // Get the reflecion value
+
+			if field.Type().Kind() == reflect.Struct {
+				if IsNullable(field) {
+					modified := field.FieldByName(MODIFIED)
+					if modified.IsValid() && modified.Kind() == reflect.Bool && modified.Bool() {
+						return true
+					}
+				}
+			}
+		}
+	}
+
+	return false
+}
+
+// ModifiedFields returns all nullable Null structs that are modified
+func ModifiedFields(input any) []string {
+
+	result := []string{}
+
+	inputValue := reflect.ValueOf(input)
+	if inputValue.Kind() == reflect.Ptr {
+		inputValue = inputValue.Elem()
+	}
+
+	if inputValue.Kind() == reflect.Struct {
+
+		inputType := reflect.TypeOf(input)
+		if inputType.Kind() == reflect.Ptr {
+			inputType = inputType.Elem()
+		}
+
+		var tag string
+		for i := 0; i < inputValue.NumField(); i++ {
+
+			field := inputType.Field(i)
+			value := inputValue.Field(i)
+
+			tag = getTag(field)
+			if tag == NOTHING {
+				tag = field.Name
+			}
+
+			if value.Type().Kind() == reflect.Struct {
+				if IsNullable(value) {
+					readOnly := value.FieldByName(READONLY)
+					if readOnly.IsValid() && readOnly.Kind() == reflect.Bool && readOnly.Bool() {
+						continue
+					}
+					modified := value.FieldByName(MODIFIED)
+					if modified.IsValid() && modified.Kind() == reflect.Bool && modified.Bool() {
+						result = append(result, tag)
+					}
+				}
+			}
+
+		}
+	}
+
+	return result
+}
+
+// SelectedFields is a generic function to get JSON tags of selected Nullable fields
+func SelectedFields[T any](input T, includeNonNullable bool) []string {
+	var fields []string
+
+	inputValue := reflect.ValueOf(input)
+	if inputValue.Kind() == reflect.Ptr {
+		inputValue = inputValue.Elem()
+	}
+
+	if inputValue.Kind() == reflect.Struct {
+
+		inputType := reflect.TypeOf(input)
+		if inputType.Kind() == reflect.Ptr {
+			inputType = inputType.Elem()
+		}
+
+		tag := NOTHING
+		for i := 0; i < inputValue.NumField(); i++ {
+
+			field := inputType.Field(i)
+			value := inputValue.Field(i)
+
+			tag = getTag(field)
+			if tag == NOTHING {
+				tag = field.Name
+			}
+
+			if value.Type().Kind() == reflect.Struct {
+				if IsNullable(value) {
+					selected := value.FieldByName(`Selected`)
+					if selected.IsValid() && selected.Kind() == reflect.Bool && selected.Bool() {
+						fields = append(fields, tag)
+					}
+				} else if includeNonNullable {
+					fields = append(fields, tag)
+				}
+			} else if includeNonNullable {
+				fields = append(fields, tag)
+			}
+
+		}
+
+	}
+
+	return fields
+}
+
+// setBooleanFields sets every field in fields to the target, all others are set to not the target.
+func setBooleanFields(instance reflect.Value, fields []string, fieldName string, target bool, not bool) error {
+	functionName := `SetNullableBooleanFields`
+	var err error
+
+	if len(fields) == 0 {
+		return nil
+	}
+
+	// Dereference pointers if necessary
+	if instance.Kind() == reflect.Ptr {
+		instance = instance.Elem()
+	}
+
+	// Ensure instance is a struct
+	if instance.Kind() != reflect.Struct {
+		m := ErrorMessage{
+			Details:  VARIABLE_MUST_BE_A_STRUCT,
+			ErrorNo:  http.StatusBadRequest,
+			Function: functionName,
+			Message:  BAD_REQUEST,
+		}
+		return m
+	}
+
+	process := false
+	value := false
+	instanceType := instance.Type()
+	for i := 0; i < instance.NumField(); i++ {
+
+		field := instanceType.Field(i)
+		tag := strings.Split(field.Tag.Get(`json`), COMMA)[0]
+		if tag == NOTHING {
+			tag = field.Name
+		}
+
+		instanceField := instance.Field(i)
+		if instanceField.Kind() == reflect.Ptr {
+			instanceField = instanceField.Elem()
+		}
+		if instanceField.Kind() != reflect.Struct {
+			continue
+		}
+		if !IsNullable(instanceField) {
+			continue
+		}
+
+		process = FieldsContainsName(fields, tag)
+		value = instanceField.FieldByName(fieldName).Bool()
+
+		if process {
+			switch {
+			case value != target: // 0
+				err = SetNullableField(target, fieldName, instanceField)
+			default: // 1
+				continue
+			}
+		}
+
+		if not && !process {
+			switch {
+			case value != target: // 0
+				continue
+			default: // 1
+				err = SetNullableField(!target, fieldName, instanceField)
+			}
+		}
+
+		if err != nil {
+			m := ErrorMessage{
+				Attempted:  `setReflectValueFromToField`,
+				Details:    "Data",
+				ErrorNo:    http.StatusInternalServerError,
+				InnerError: err,
+				Message:    UNEXPECTED_ERROR,
+			}
+			return m
+		}
+	}
+
+	return nil
+}
+
+// SetModifiedBooleanFields calls SetBooleanFields with 'Modified' as the field name
+func SetModifiedBooleanFields(instance reflect.Value, fields []string, target bool, not bool) error {
+	return setBooleanFields(instance, fields, `Modified`, target, not)
+}
+
+// SetModifiedIfSelected is used for data that
+// is unmarshalled but needs to be saved.
+func SetModifiedIfSelected(model any) error {
+
+	function := `SetModifiedIfSelected`
+
+	// Get the type and inputValue of the input data
+	inputValue := reflect.ValueOf(model)
+
+	if inputValue.Kind() == reflect.Ptr {
+		inputValue = inputValue.Elem()
+	}
+
+	if inputValue.Kind() != reflect.Struct {
+		return nil
+	}
+
+	structType := inputValue.Type()
+
+	for i := 0; i < inputValue.NumField(); i++ {
+		field := inputValue.Field(i)
+		fieldName := structType.Field(i).Name
+
+		if field.Type().Kind() != reflect.Struct {
+			continue
+		}
+
+		if !IsNullable(field) {
+			continue
+		}
+
+		selected := field.FieldByName("Selected")
+		modified := field.FieldByName("Modified")
+
+		// Ensure fields are valid and settable
+		if selected.IsValid() && selected.Kind() == reflect.Bool && selected.Bool() {
+			if modified.IsValid() && modified.Kind() == reflect.Bool {
+				err := SetNullableField(true, `Modified`, field)
+				if err != nil {
+					m := ErrorMessage{
+						Attempted: `SetNullableField`,
+						Details:   fmt.Sprintf(`FieldName: %s Err: %+v`, fieldName, err),
+						ErrorNo:   http.StatusInternalServerError,
+						Function:  function,
+						Message:   UNEXPECTED_ERROR,
+					}
+					return m
+				}
+			}
+		}
+
+	}
+
+	return nil
+}
+
+// SetNullableField sets the value to a specific field in a Nullable struct
+func SetNullableField(value any, fieldName string, nullableField reflect.Value) error {
+	if nullableField.Kind() == reflect.Ptr {
+		nullableField = nullableField.Elem()
+	}
+
+	if nullableField.Kind() != reflect.Struct {
+		return fmt.Errorf("nullableField is not a struct")
+	}
+
+	field := nullableField.FieldByName(fieldName)
+	if !field.IsValid() {
+		return fmt.Errorf("no such field: %s in struct", fieldName)
+	}
+
+	if !field.CanSet() {
+		return fmt.Errorf("cannot set field %s", fieldName)
+	}
+
+	val := reflect.ValueOf(value)
+
+	switch fieldName {
+	case "Data":
+		if field.Type() != val.Type() {
+			return fmt.Errorf("value type %s does not match field type %s", val.Type(), field.Type())
+		}
+		field.Set(val)
+	case "Valid", "Selected", "Modified":
+		if val.Kind() != reflect.Bool {
+			return fmt.Errorf("value type %s does not match field type bool", val.Kind())
+		}
+		field.SetBool(val.Bool())
+	default:
+		return fmt.Errorf("unsupported field name: %s", fieldName)
+	}
+
+	return nil
+}
+
+// SetReadOnlyBooleanFields calls SetBooleanFields with "ReadOnly" as the field name
+func SetReadOnlyBooleanFields(instance reflect.Value, fields []string, target bool, not bool) error {
+	return setBooleanFields(instance, fields, READONLY, target, not)
+}
+
+// setReflectValueFromToField sets the value from the `from` struct to the `to` struct for the specified field.
+func setReflectValueFromToField(from, to reflect.Value, field string) error {
+	// Dereference the pointers if necessary
+	if from.Kind() == reflect.Ptr {
+		from = from.Elem()
+	}
+	if to.Kind() == reflect.Ptr {
+		to = to.Elem()
+	}
+
+	// Ensure both from and to are structs
+	if from.Kind() != reflect.Struct || to.Kind() != reflect.Struct {
+		return errors.New("both 'from' and 'to' must be structs or pointers to structs")
+	}
+
+	// Get the field from both structs
+	fromField := from.FieldByName(field)
+	toField := to.FieldByName(field)
+
+	// Check if the field exists in both structs
+	if !fromField.IsValid() {
+		return fmt.Errorf("field %s not found in 'from' struct", field)
+	}
+	if !toField.IsValid() {
+		return fmt.Errorf("field %s not found in 'to' struct", field)
+	}
+
+	// Ensure the toField is settable
+	if !toField.CanSet() {
+		return fmt.Errorf("field %s cannot be set in 'to' struct", field)
+	}
+
+	// Set the value
+	toField.Set(fromField)
+	return nil
+}
+
+// SetSelectedBooleanFields calls SetBooleanFields with "Selected" as the field name
+func SetSelectedBooleanFields(instance reflect.Value, fields []string, target bool, not bool) error {
+	return setBooleanFields(instance, fields, `Selected`, target, not)
 }
 
 // Set assigns a Nullable[model] as well as selected and valid.
